53692e97f00c008f5b7e8aaadb451324f8ad1910
Dennis Emil <dennisemil@gmail.com>
2016-11-22 13:14:22 +0100

    css for info

--- application/views/funnybunny_view.php
+++ application/views/funnybunny_view.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
